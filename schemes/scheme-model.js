0ff94998fe33dd9920725856361ba123110cfac5
Justin Russell <Justin@Justins-MacBook.local>
2020-04-15 15:10:26 -0700

    mvp and stretch done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -7,6 +7,7 @@ module.exports = {
     add,
     update,
     remove,
+    addStep,
 };
 
 function find() {
@@ -45,4 +46,15 @@ function remove(id) {
     return db('schemes')
         .where({ id })
         .del();
-}
+};
+
+function addStep(step, scheme_id) {
+    const newStep = {
+        ...step,
+        scheme_id
+    }
+    return db('steps')
+        .insert(newStep)
+        .then(id => db('steps')
+        .where({ id: id[0] }))
+};
