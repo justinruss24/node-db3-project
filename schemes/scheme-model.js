ff5cf06205df165c330d3392a7c3d30d3865969b
Justin Russell <Justin@Justins-MacBook.local>
2020-04-15 14:45:26 -0700

    3/6 modules done

---
+++ schemes/scheme-model.js
@@ -0,0 +1,22 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    // add,
+    // update,
+    // remove,
+};
+
+function find() {
+    return db('schemes');
+};
+
+function findById(id) {
+    return db('schemes').where({ id }).first();
+};
+
+function findSteps(id) {
+    return db('steps').where({ id })
+};
