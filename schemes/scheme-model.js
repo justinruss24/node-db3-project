c16062cb86376583ae25320b1ccb7593e160860e
Justin Russell <Justin@Justins-MacBook.local>
2020-04-15 15:07:55 -0700

    all db methods done

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,9 +4,9 @@ module.exports = {
     find,
     findById,
     findSteps,
-    // add,
-    // update,
-    // remove,
+    add,
+    update,
+    remove,
 };
 
 function find() {
@@ -18,5 +18,31 @@ function findById(id) {
 };
 
 function findSteps(id) {
-    return db('steps').where({ id })
+    return db('schemes')
+        .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+        .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+        .where({ 'schemes.id': id })
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, "id")
+    .then(([id]) => {
+        return findById(id)
+    })
 };
+
+function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+        .then(() => {
+            return findById(id);
+        })
+};
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
+}
